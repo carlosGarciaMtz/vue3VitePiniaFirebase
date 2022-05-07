4ad0fa538ca78fc30d79ef133d8dc1b2bbe77069
CarlosGarciaMtz <carlos.cg242@gmail.com>
2022-05-07 01:14:00 -0500

    config firestore/lite

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZD1n9dCtS-FMY9KbPgD78rLmklJd9Bxo",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 const auth = getAuth()
+const db = getFirestore();
 
-export { auth }
+export { auth, db }
